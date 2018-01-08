530b7404fd27e4f11ada70fd7da770a2aeff748a
陈林泳 <clinyong@gmail.com>
2018-01-08 13:13:10 +0800

    add header

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -14,72 +14,93 @@ html, body {
 }
 `;
 
-const Container = styled.div`position: relative; font`;
+const Container = styled.div`position: relative;`;
 const Content = styled.div`
-	@media screen and (min-width: 770px) {
-		padding: 40px 0 100px;
-	}
+    @media screen and (min-width: 770px) {
+        padding: 40px 0 100px;
+    }
+`;
+
+const Header = styled.ul`
+    text-align: center;
+    margin-right: 30px;
+    margin-bottom: 30px;
+    margin-top: 30px;
+
+    &>a {
+        color: #222;
+        text-align: center;
+        text-decoration: none;
+        font-size: 18px;
+    }
 `;
 
 const Footer = styled.div`
-	position: absolute;
-	bottom: 20px;
-	left: 0;
-	right: 0;
-	text-align: center;
-	font-size: 12px;
+    position: absolute;
+    bottom: 20px;
+    left: 0;
+    right: 0;
+    text-align: center;
+    font-size: 12px;
 `;
 
 const Coding = styled.span`
-	color: #999;
+    color: #999;
 
-	& > a {
-		color: #108ee9 !important;
-	}
+    & > a {
+        color: #108ee9 !important;
+    }
 `;
 
 export default class Layout extends React.PureComponent<{}, {}> {
-	componentDidMount() {
-		function injectGA() {
-			(function(i, s, o, g, r, a, m) {
-				i["GoogleAnalyticsObject"] = r;
-				(i[r] =
-					i[r] ||
-					function() {
-						(i[r].q = i[r].q || []).push(arguments);
-					}),
-					(i[r].l = 1 * new Date());
-				(a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-				a.async = 1;
-				a.src = g;
-				m.parentNode.insertBefore(a, m);
-			})(
-				window,
-				document,
-				"script",
-				"https://www.google-analytics.com/analytics.js",
-				"ga"
-			);
+    componentDidMount() {
+        function injectGA() {
+            (function(i, s, o, g, r, a, m) {
+                i["GoogleAnalyticsObject"] = r;
+                (i[r] =
+                    i[r] ||
+                    function() {
+                        (i[r].q = i[r].q || []).push(arguments);
+                    }),
+                    (i[r].l = 1 * new Date());
+                (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m);
+            })(
+                window,
+                document,
+                "script",
+                "https://www.google-analytics.com/analytics.js",
+                "ga"
+            );
 
-			ga("create", "UA-91794310-1", "auto");
-			ga("send", "pageview");
-		}
-		if (process.env.NODE_ENV === "production") {
-			injectGA();
-		}
-	}
+            ga("create", "UA-91794310-1", "auto");
+            ga("send", "pageview");
+        }
+        if (process.env.NODE_ENV === "production") {
+            injectGA();
+        }
+    }
 
-	render() {
-		return (
-			<Container>
-				<Content>{this.props.children}</Content>
-				<Footer>
-					<Coding>
-						Hosted by{" "}
-						<a href="https://pages.coding.me">Coding Pages</a>
-					</Coding>
-				</Footer>
-			</Container>
-		);
-	}
+    render() {
+        return (
+            <Container>
+                <Header>
+                    <a href="/">首页</a>
+                    <a href="/archive.html" style={{ margin: "0 50px" }}>
+                        归档
+                    </a>
+                    <a href="/about.html">关于</a>
+                </Header>
+                <Content>{this.props.children}</Content>
+                <Footer>
+                    <Coding>
+                        Hosted by{" "}
+                        <a href="https://pages.coding.me">Coding Pages</a>
+                    </Coding>
+                </Footer>
+            </Container>
+        );
+    }
 }
