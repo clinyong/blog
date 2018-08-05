466c1a715a69a9f89730fa3b5bd03d6495a327f7
clinyong <clinyong@gmail.com>
2018-08-05 18:55:32 +0800

    Fix check in viewport

--- src/view/Post.tsx
+++ src/view/Post.tsx
@@ -30,12 +30,8 @@ function lazyLoadDisqus() {
     function isInViewport(element) {
         const rect = element.getBoundingClientRect();
         const html = document.documentElement;
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || html.clientHeight) &&
-            rect.right <= (window.innerWidth || html.clientWidth)
-        );
+
+        return rect.top < (window.innerHeight || html.clientHeight);
     }
     function injectDisqus() {
         const d = document,
